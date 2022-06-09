f1d44c601782377572775cf499f7aa10d404f692
Hugo Vieira <academicohcv@gmail.com>
2022-06-09 17:59:23 -0300

    UI improvements

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 export class SearchBarComponent implements OnInit {
 
   constructor(private router: Router) { }
-
+  search: string = '';
   ngOnInit(): void {
   }
 
