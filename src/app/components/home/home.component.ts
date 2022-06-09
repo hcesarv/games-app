dd55e9f165db6b8130feb4d423b5ba1a17d2c3f4
Hugo Vieira <academicohcv@gmail.com>
2022-06-09 18:24:26 -0300

    details component created

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { APIResponse, Game } from 'src/app/models';
 import { HttpService } from 'src/app/services/http.service';
 
@@ -8,18 +9,21 @@ import { HttpService } from 'src/app/services/http.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   sort: string = '';
   games!: Array<Game>;
+  private routeSub!: Subscription;
+  private gameSub!: Subscription;
 
   constructor(
     private httpService: HttpService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params) => {
-      if(params['game-search']) {
+    this.routeSub = this.route.params.subscribe((params: Params) => {
+      if (params['game-search']) {
         this.searchGames('metacrit', params['game-search']);
       } else {
         this.searchGames('metacrit');
@@ -28,10 +32,18 @@ export class HomeComponent implements OnInit {
   }
 
   searchGames(sort: string, search?: string): void {
-    this.httpService.getGamesList(sort, search).subscribe((gameList: APIResponse<Game>) => {
+    this.gameSub = this.httpService.getGamesList(sort, search).subscribe((gameList: APIResponse<Game>) => {
       this.games = gameList.results;
-      console.log(gameList.results)
     });
+  }
+
+  openGameDetails(id: string): void {
+    this.router.navigate(['details', id]);
+  }
+
+  ngOnDestroy(): void {
+    if (this.gameSub) this.gameSub.unsubscribe();
+    if (this.routeSub) this.routeSub.unsubscribe();
   }
 
 }
